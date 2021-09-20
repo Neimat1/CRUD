8f9889198c51809b84952d69110c569e26da0bc4
Neimat Soliman <neimat.soliman.ismail@gmail.com>
2021-09-21 01:28:14 +0200

    add function  valid

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ var productPrice        = document.getElementById('productPrice');
 var productCategory     = document.getElementById('productCategory');
 var productDescription  = document.getElementById('productDescription');
 var buttonAddOrUpdate   = document.getElementById('button-add-update');
+var productSearch       = document.getElementById('search');
+
 var currentIndex = 0;
+var isValid=true;
 ////////////////////////////////////////////////////////////////////////// 
 ////////////////////////////////////////////////////////////////////////// 
 //var products = [];
@@ -21,18 +24,90 @@ var products;
 
 //////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////// 
-//main function that call other functions
-function main(){
-    if( !isEmpty()){
-        if((buttonAddOrUpdate.innerHTML).includes("Update")){
-            updateProduct(currentIndex);
-        }
-        else if(buttonAddOrUpdate.innerHTML==='Add Product'){
-            addProduct();
-        }
-        clearInputs();
-        displayProduct();
+buttonAddOrUpdate.addEventListener('click',function(){
+    if((buttonAddOrUpdate.innerHTML).includes("Update")&&valid()){
+        updateProduct(currentIndex);
     }
+    else if(buttonAddOrUpdate.innerHTML.includes('Add Product')&&valid()){
+        addProduct();
+    }
+    clearInputs();
+    displayProduct();
+})
+////////////////////////////////////////////////////////////////////////// 
+////////////////////////////////////////////////////////////////////////// 
+function validateProductName(){
+    regex = /^[A-Z][A-Za-z ]{3,20}$/;
+    if(regex.test(productName.value)){
+        productName.classList.add('is-valid');
+        productName.classList.remove('is-invalid');
+        return true;
+    }
+    else{   
+        productName.classList.add('is-invalid');
+        productName.classList.remove('is-valid');
+        return false;
+    }
+}
+productName.addEventListener('keyup',validateProductName);
+
+function validateProductPrice(){
+    regex =/^[1-9][0-9]{0,9}$/;
+    if(regex.test(productPrice.value)){
+        productPrice.classList.add('is-valid');
+        productPrice.classList.remove('is-invalid');
+        return true;
+    }
+    else{   
+        productPrice.classList.add('is-invalid');
+        productPrice.classList.remove('is-valid');
+        return false;
+    }
+}
+productPrice.addEventListener('keyup',validateProductPrice);
+
+function validateProductCategory(){
+    regex = /^[A-Z][A-Za-z ]{3,20}$/;
+    if(regex.test(productCategory.value)){
+        productCategory.classList.add('is-valid');
+        productCategory.classList.remove('is-invalid');
+        return true;
+    }
+    else{   
+        productCategory.classList.add('is-invalid');
+        productCategory.classList.remove('is-valid');
+        return false;
+    }
+}
+productCategory.addEventListener('keyup',validateProductCategory);
+
+
+function validateProductDescription(){
+    regex = /^[A-Z][A-Za-z ]{3,500}$/;
+    if(regex.test(productDescription.value)){
+        productDescription.classList.add('is-valid');
+        productDescription.classList.remove('is-invalid');
+        return true;
+    }
+    else{   
+        productDescription.classList.add('is-invalid');
+        productDescription.classList.remove('is-valid');
+        return false;
+    }
+}
+productDescription.addEventListener('keyup',validateProductDescription);
+
+function valid(){
+    if(validateProductName()&&
+        validateProductPrice()&&
+        validateProductCategory()&&
+        validateProductDescription()){
+            return true
+        }
+        else{
+            return false
+        }
+
 }
 ////////////////////////////////////////////////////////////////////////// 
 ////////////////////////////////////////////////////////////////////////// 
@@ -63,6 +138,10 @@ function clearInputs(){
     productPrice.value        = "";
     productCategory.value     = "";
     productDescription.value  = "";
+    productName.classList.remove('is-valid');
+    productPrice.classList.remove('is-valid');
+    productCategory.classList.remove('is-valid');
+    productDescription.classList.remove('is-valid');
 }
 ////////////////////////////////////////////////////////////////////////// 
 ////////////////////////////////////////////////////////////////////////// 
@@ -91,22 +170,6 @@ function displayProductData(indexOfProduct, temporaryContainter ){
     `
     return temporaryContainter;
 }
-////////////////////////////////////////////////////////////////////////// 
-////////////////////////////////////////////////////////////////////////// 
-
-function isEmpty(){
-    if(
-        (productName.value== "")&&
-        (productPrice.value== "")&& 
-        (productCategory.value== "")&&
-        (productDescription.value== "")
-    ){
-        return true;
-    }
-    else
-        return false;
-
-}
 
 ////////////////////////////////////////////////////////////////////////// 
 ////////////////////////////////////////////////////////////////////////// 
@@ -114,14 +177,15 @@ function searchProduct(product){
     var temporaryContainter=``;
     for(var i=0;i<products.length;i++){
         if(
-            (products[i].name.toLowerCase().includes(product))||
-            (products[i].category.toLowerCase().includes(product))
+            (products[i].name.toUpperCase().includes(product.toUpperCase()))||
+            (products[i].category.toUpperCase().includes(product.toUpperCase()))
             ){
                 temporaryContainter =  displayProductData(i, temporaryContainter);
         }
     }
     document.getElementById('tbody').innerHTML=temporaryContainter;
 }
+// productSearch.addEventListener('keyup',searchProduct(productSearch.value));
 ////////////////////////////////////////////////////////////////////////// 
 ////////////////////////////////////////////////////////////////////////// 
 function DisplayCurrentUpdateProduct(indexOfProduct){
